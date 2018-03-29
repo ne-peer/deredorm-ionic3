e3e33ad482ad8bfc9af8cb9e1acb2eddb442be4b
ne-peer <mileage0110@gmail.com>
2018-03-29 14:28:11 +0900

    [update] avaterHostをpublicに&formatting

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,31 +1,39 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
-import { StarlightCard } from '../../services/client/starlight-card.service';
-import { Card } from '../../models/starlightdb/card';
-import { Hosts } from '../../constants/host';
+import { Component, OnInit } from "@angular/core";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  LoadingController
+} from "ionic-angular";
+import { StarlightCard } from "../../services/client/starlight-card.service";
+import { Card } from "../../models/starlightdb/card";
+import { Hosts } from "../../constants/host";
 
 @IonicPage()
 @Component({
-  selector: 'page-detail',
-  templateUrl: 'detail.html',
+  selector: "page-detail",
+  templateUrl: "detail.html",
   providers: [StarlightCard]
 })
 export class DetailPage implements OnInit {
-
+  public avaterHost: string;
   private chara_id: number;
   private card: Card;
-  private avaterHost: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private client: StarlightCard,
-    private loadingCtrl: LoadingController) {
-    this.chara_id = navParams.get('item');
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private client: StarlightCard,
+    private loadingCtrl: LoadingController
+  ) {
+    this.chara_id = navParams.get("item");
     console.log(this.chara_id);
     this.avaterHost = Hosts.STARLIGHT_AVATER;
   }
 
   ngOnInit() {
     (async () => {
-      let loading = this.loadingCtrl.create({ content: '読み込み中...' });
+      let loading = this.loadingCtrl.create({ content: "読み込み中..." });
       await loading.present();
 
       await this.client.fetch(this.chara_id, false);
@@ -35,5 +43,4 @@ export class DetailPage implements OnInit {
       await loading.dismiss();
     })();
   }
-
 }
--- src/pages/types/types.ts
+++ src/pages/types/types.ts
@@ -1,21 +1,20 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Char } from '../../models/starlightdb/char';
-import { Hosts } from '../../constants/host';
-import { DetailPage } from '../detail/detail';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Char } from "../../models/starlightdb/char";
+import { Hosts } from "../../constants/host";
+import { DetailPage } from "../detail/detail";
 
 @IonicPage()
 @Component({
-  selector: 'page-types',
-  templateUrl: 'types.html',
+  selector: "page-types",
+  templateUrl: "types.html"
 })
 export class TypesPage {
-
+  public avaterHost: string;
   private char: Char;
-  private avaterHost: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.char = navParams.get('item');
+    this.char = navParams.get("item");
     this.avaterHost = Hosts.STARLIGHT_AVATER;
     console.log(this.char);
   }
@@ -25,5 +24,4 @@ export class TypesPage {
       item: cardNumber
     });
   }
-
 }
